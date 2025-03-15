bce744a93de26a186672c266136e464ebeae3c98
Pietro Peterlongo <pietro.peterlongo@agilelab.com>
2025-03-15 16:09:07 +0100

    basic version (no sugar) with text and code (not executed and no output capture) working

--- src/mib/__init__.py
+++ src/mib/__init__.py
@@ -1,8 +1,10 @@
 from pydantic import BaseModel, RootModel
 import json
+import markdown
+import os, inspect
+
 
 class Block(BaseModel):
-    
     def to_json(self) -> str:
         return self.model_dump_json()
 
@@ -11,9 +13,22 @@ class Code(Block):
     code: str = ""
     output: str = ""
 
+    def to_html(self) -> str:
+        return f"""
+<pre><code class="python">
+{self.code}
+</code></pre>
+<pre>
+{self.output}
+</pre>
+"""
+
 
 class Text(Block):
     text: str = ""
+
+    def to_html(self) -> str:
+        return markdown.markdown(self.text)
 
 
 class Doc(Block):
@@ -24,3 +39,19 @@ class Doc(Block):
 
     def to_json(self) -> str:
         return json.dumps([blk.to_json() for blk in self.blocks])
+
+    def to_html(self) -> str:
+        return f"""
+<html>
+<head>
+</head>
+<body>
+  {"\n".join([blk.to_html() for blk in self.blocks])}
+</body>
+</html>
+"""
+
+    def save(self):
+        filename = inspect.stack()[1].filename.replace(".py", ".html")
+        with open(filename, "w") as f:
+            f.write(self.to_html())
